96e02fcdd4079c729149c4a2e4f786f5423d2b49
Jadejaguar <imanzahid7390@gmail.com>
2026-05-17 23:48:28 -0400

    Complete WEB 102 prework with walkthrough video, bonus features and visual redesign

--- README.md
+++ README.md
@@ -1,53 +1,38 @@
-# WEB102 Prework - *Name of App Here*
+# WEB102 Prework - *Sea Monster Crowdfunding*
 
-Submitted by: **Your Name Here**
+Submitted by: **Iman Zahid**
 
-**Name of your app** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
+**Sea Monster Crowdfunding** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
 
-Time spent: **X** hours spent in total
+Time spent: **4** hours spent in total
 
 ## Required Features
 
 The following **required** functionality is completed:
 
-* [ ] The introduction section explains the background of the company and how many games remain unfunded.
-* [ ] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
-* [ ] The Our Games section initially displays all games funded by Sea Monster Crowdfunding
-* [ ] The Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
+* [x] The introduction section explains the background of the company and how many games remain unfunded.
+* [x] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
+* [x] The Our Games section initially displays all games funded by Sea Monster Crowdfunding
+* [x] The Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
 
 The following **optional** features are implemented:
 
-* [ ] List anything else that you can get done to improve the app functionality!
+* [x] Added a search bar above the games that filters them by name as you type. It's not case sensitive so it works no matter how you type it.
+* [x] If your search doesn't match any game, instead of leaving the section blank, a message shows up saying no games were found and it includes whatever you typed.
+* [x] Added a sticky nav bar at the top with links that jump to the Welcome, Stats, and Our Games sections. It also scrolls smoothly instead of just teleporting to the section.
+* [x] Changed the whole color scheme to a Deep Ocean palette. The background is dark navy, the accents are teal, and the stats cards are a midnight blue color.
+* [x] Made the text bigger and changed up the styles. The headings are bolder with teal underlines, the stat labels are uppercase with letter spacing, and the description is in italics now.
+* [x] The important numbers in the description (like the total raised, the total number of games, and the unfunded count) are bolded and colored teal so they stand out from the rest of the text.
 
 ## Video Walkthrough
 
 Here's a walkthrough of implemented features:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<!-- walkthrough.mp4 
+    edit README on github -->
 
-<!-- Replace this with whatever GIF tool you used! -->
-GIF created with ...  
-<!-- Recommended tools:
-[Kap](https://getkap.co/) for macOS
-[ScreenToGif](https://www.screentogif.com/) for Windows
-[peek](https://github.com/phw/peek) for Linux. -->
+Video recorded with Zoom.
 
 ## Notes
 
-Describe any challenges encountered while building the app.
-
-## License
-
-    Copyright [yyyy] [name of copyright owner]
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+It was a little hard because I kinda forgot how to use the array methods, especially reduce since I couldn't remember how the accumulator works.The bonus features like the search bar was actually pretty easy because I could just reuse the addGamesToPage and deleteChildElements functions that were already there. The visual redesign took the longest since picking a color palette and figuring out the text hierarchy was harder than I thought it would be.
--- index.js
+++ index.js
@@ -29,26 +29,34 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for (let i = 0; i < games.length; i++) {
 
         // create a new div element, which will become the game card
-
+        const gameCard = document.createElement("div");
 
         // add the class game-card to the list
+        gameCard.classList.add("game-card");
 
-
-        // set the inner HTML using a template literal to display some info 
+        // set the inner HTML using a template literal to display some info
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
+        gameCard.innerHTML = `
+            <img src="${games[i].img}" class="game-img" />
+            <h2>${games[i].name}</h2>
+            <p>${games[i].description}</p>
+            <p>Backers: ${games[i].backers}</p>
+        `;
 
         // append the game to the games-container
+        gamesContainer.appendChild(gameCard);
+    }
 
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
+addGamesToPage(GAMES_JSON);
 
 
 /*************************************************************************************
@@ -61,19 +69,23 @@ function addGamesToPage(games) {
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce((acc, game) => acc + game.backers, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
+contributionsCard.innerHTML = `${totalContributions.toLocaleString()}`;
 
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+const totalRaised = GAMES_JSON.reduce((acc, game) => acc + game.pledged, 0);
 
 // set inner HTML using template literal
+raisedCard.innerHTML = `$${totalRaised.toLocaleString()}`;
 
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+gamesCard.innerHTML = `${GAMES_JSON.length}`;
 
 
 /*************************************************************************************
@@ -87,10 +99,12 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    const unfundedGames = GAMES_JSON.filter((game) => game.pledged < game.goal);
+    
+    // console.log("Number of unfunded games:", unfundedGames.length);
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unfundedGames);
 }
 
 // show only games that are fully funded
@@ -98,10 +112,10 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    const fundedGames = GAMES_JSON.filter((game) => game.pledged >= game.goal);
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(fundedGames);
 }
 
 // show all games
@@ -109,7 +123,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -118,6 +132,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 
 /*************************************************************************************
@@ -129,12 +146,24 @@ const allBtn = document.getElementById("all-btn");
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
+const numUnfundedGames = GAMES_JSON.filter((game) => game.pledged < game.goal).length;
+/*
+const numUnfundedGames = GAMES_JSON.reduce((acc, game) => {
+    return game.pledged < game.goal ? acc + 1 : acc;
+}, 0);
+*/
 
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+// the <strong> tags here are styled teal + non-italic by the CSS so the key numbers pop
+const displayStr = `A total of <strong>$${totalRaised.toLocaleString()}</strong> has been raised for
+<strong>${GAMES_JSON.length}</strong> games. Currently, <strong>${numUnfundedGames}</strong> ${numUnfundedGames === 1 ?
+"game remains" : "games remain"} unfunded. We need your help to fund these amazing games!`;
 
 // create a new DOM element containing the template string and append it to the description container
+const descriptionPara = document.createElement("p");
+descriptionPara.innerHTML = displayStr;
+descriptionContainer.appendChild(descriptionPara);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
@@ -149,7 +178,56 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [firstGame, secondGame, ...otherGames] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const firstGameName = document.createElement("p");
+firstGameName.innerHTML = firstGame.name;
+firstGameContainer.appendChild(firstGameName);
 
 // do the same for the runner up item
+const secondGameName = document.createElement("p");
+secondGameName.innerHTML = secondGame.name;
+secondGameContainer.appendChild(secondGameName);
+
+
+//filterUnfundedOnly();
+//filterFundedOnly();
+
+
+/*************************************************************************************
+ * Bonus Feature: Live search bar
+ * Filters game cards by name as the user types. Shows a friendly
+ * "no results" message when the query matches zero games.
+ * Skills used: events, filter, includes, template literals, DOM manipulation
+*/
+
+// grab the search input we added in index.html
+const searchInput = document.getElementById("search-input");
+
+// the "input" event fires on every keystroke (better than "change" for live filtering)
+searchInput.addEventListener("input", (event) => {
+    // lowercase both sides of the comparison so the search is case-insensitive
+    const query = event.target.value.toLowerCase();
+
+    // keep only games whose name contains the query string
+    // (when query is "", every string includes it, so all games show)
+    const matchingGames = GAMES_JSON.filter((game) =>
+        game.name.toLowerCase().includes(query)
+    );
+
+    // clear out whatever cards are currently shown
+    deleteChildElements(gamesContainer);
+
+    if (matchingGames.length === 0) {
+        // no matches: show a custom message instead of leaving the section blank
+        const noResults = document.createElement("p");
+        noResults.classList.add("no-results");
+        // echo back the original (non-lowercased) query so capitalization is preserved
+        noResults.innerHTML = `No games found matching "<strong>${event.target.value}</strong>". Try a different search!`;
+        gamesContainer.appendChild(noResults);
+    } else {
+        // we have matches: render them using the existing card-building function
+        addGamesToPage(matchingGames);
+    }
+});
